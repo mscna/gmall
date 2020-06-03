3a4a7833848729046ea13aefbd17caf4abfdfbd8
mscna <motongwei@hotmail.com>
2020-06-03 23:50:19 +0800

    项目初始化...

---
+++ gmall-pms/src/main/java/com/wc/gmall/gms/GmallPmsApplication.java
@@ -0,0 +1,13 @@
+package com.wc.gmall.gms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallPmsApplication.class, args);
+    }
+
+}
